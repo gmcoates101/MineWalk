626a6a295684023bd2544f694c45e92cad4a25aa
gmcoates101 <gmcoates@mac.com>
2022-03-08 12:49:43 +0000

    Initial Setup

---
+++ MineWalk.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MineWalk.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
